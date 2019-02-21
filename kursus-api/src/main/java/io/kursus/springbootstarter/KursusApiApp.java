9935e0d04a278735359b431edf7625c4be3bd00a
Jesper Trampedach <jesper@trampedach.dk>
2019-02-21 18:03:22 +0100

    new

---
+++ kursus-api/src/main/java/io/kursus/springbootstarter/KursusApiApp.java
@@ -0,0 +1,18 @@
+package io.kursus.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class KursusApiApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KursusApiApp.class,args);
+		
+		
+	}
+
+}
